6cf0de915848a9078689a606e1f00089f93d191a
MattyLake <matthew.lakin6@gmail.com>
2024-11-29 15:56:02 +0000

    Added TODO

--- bfs.py
+++ bfs.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def changeToStandardForm(c,A,b,signs):
     # TODO: Validation of input
+    # TODO: Add additional rows for artificial variables and slack variables
 
     basicIndicies = np.array([])
     artificalIndicies = np.array([])
@@ -36,17 +37,18 @@ def changeToStandardForm(c,A,b,signs):
                     basicIndicies = np.append(basicIndicies, len(A[1]) - 1)
                     artificalIndicies = np.append(artificalIndicies, len(A[1]) - 1)
 
-    return c,A,b,basicIndicies,artificalIndicies
+    return c,A,b,basicIndicies.astype(int),artificalIndicies.astype(int)
 
 
-c = -np.array([1, 4, 7, 5])
-A = np.array([[2, 1, 2, 4], [3, -1, -2, 6]])
-b = np.array([10, 5])
-signs = np.array([-1,0]) # 1 is >= , -1 is <= , 0 is =
-
-c,A,b,basicIndicies,artificalIndicies = changeToStandardForm(c,A,b,signs)
-
-
-print(A)
-print(basicIndicies)
-print(artificalIndicies)
+# nature = -1  # 1 is minimization, -1 is maximization
+# c = nature * np.array([1, 4, 7, 5])
+# A = np.array([[2, 1, 2, 4], [3, -1, -2, 6]])
+# b = np.array([10, 5])
+# signs = np.array([-1,0])  # 1 is >= , -1 is <= , 0 is =
+#
+# c,A,b,basicIndicies,artificalIndicies = changeToStandardForm(c,A,b,signs)
+#
+#
+# print(A)
+# print(basicIndicies)
+# print(artificalIndicies)
