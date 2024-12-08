8e5132e09d999346e05853d00d14eeabbcb99ccc
JamesDeath <J.death-22@student.lboro.ac.uk>
2024-12-08 14:28:39 +0000

    update package
    changed package input to cw question

--- simplexPackage.py
+++ simplexPackage.py
@@ -1,14 +1,14 @@
 import numpy as np
 from scipy.optimize import linprog
 
-c =-np.array([7, 6, 0, 0])
+c =np.array([7, 0, 11, -10,-1,26])
 
 #define Aup,bup with constraints containing <= and Aeq,beq with constraints containing =
-Aup=None
-bup=None
-Aeq = np.array([[2, 4, 1, 0], [3, 2, 0, 1]])
-beq = np.array([16, 12])
+Aup=np.array([[0,1,-1,1,0,3],[1,1,-3,1,1,0],[-1,-1,0,0,0,-1]])
+bup=([18,12,-50])
+Aeq = np.array([[1, -1, 1, 0,1,1]])
+beq = np.array([76])
 
 
 res=linprog(c,Aup,bup,Aeq,beq)
-print(-res.fun)
+print(res.fun)
