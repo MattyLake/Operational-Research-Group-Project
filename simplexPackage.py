c492d2b808c42266671313243608cbb73b361e52
MattyLake <matthew.lakin6@gmail.com>
2024-12-08 23:18:54 +0000

    Clean simplexPackage.py to keep it inline with report

--- simplexPackage.py
+++ simplexPackage.py
@@ -1,14 +1,14 @@
 import numpy as np
 from scipy.optimize import linprog
 
-c =np.array([7, 0, 11, -10,-1,26])
+c = np.array([7, 0, 11, -10,-1,26])
 
 #define Aup,bup with constraints containing <= and Aeq,beq with constraints containing =
 Aup=np.array([[0,1,-1,1,0,3],[1,1,-3,1,1,0],[-1,-1,0,0,0,-1]])
-bup=([18,12,-50])
+bup= np.array([18,12,-50])
 Aeq = np.array([[1, -1, 1, 0,1,1]])
 beq = np.array([76])
 
 
-res=linprog(c,Aup,bup,Aeq,beq)
-print(res.fun)
+res = linprog(c,Aup,bup,Aeq,beq)
+print(res)
